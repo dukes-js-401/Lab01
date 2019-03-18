2a122c67b042850e48df81089a1669a3cfaae1e7
Gregory Dukes <kcils360@gmail.com>
2019-03-18 16:00:50 -0700

    re-wrote tests to include all value types that return null

--- __tests__/greet.test.js
+++ __tests__/greet.test.js
@@ -4,11 +4,46 @@ const greet = require('../lib/greet.js');
 
 describe('Greet', () => {
 
-    it('Returns null if passed is not a string', () => {
-        let message = greet(123);
-        let expectedOutput = null;
-        expect(expectedOutput).toEqual(null);
+    describe('Returns null if passed not a typeof string', () => {
+
+        it('Returns null if passed int', () => {
+            let message = greet(123);
+            let expectedOutput = null;
+            expect(expectedOutput).toEqual(null);
+        })
+        it('Returns null if passed bool true', () => {
+            let message = greet(true);
+            let expectedOutput = null;
+            expect(expectedOutput).toEqual(null);
+        })
+        it('Returns null if passed bool false', () => {
+            let message = greet(false);
+            let expectedOutput = null;
+            expect(expectedOutput).toEqual(null);
+        })
+        it('Returns null if passed null', () => {
+            let message = greet(null);
+            let expectedOutput = null;
+            expect(expectedOutput).toEqual(null);
+        })
+        it('Returns null if passed symbol', () => {
+            const symbol = Symbol();
+            let message = greet(symbol);
+            let expectedOutput = null;
+            expect(expectedOutput).toEqual(null);
+        })
+        it('Returns null if passed array', () => {
+            let message = greet([1,2,3]);
+            let expectedOutput = null;
+            expect(expectedOutput).toEqual(null);
+        })
+        it('Returns null if passed object', () => {            
+            let message = greet({});
+            let expectedOutput = null;
+            expect(expectedOutput).toEqual(null);
+        })
     })
+
 
     it('return hello world when given world', () => {
         let message = greet('World');
