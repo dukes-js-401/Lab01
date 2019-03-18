65adb2d3b1eb47fbc01de57317515165712e3050
Gregory Dukes <kcils360@gmail.com>
2019-03-18 16:14:27 -0700

    basic tests written for add and subtract arithmatic

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -2,3 +2,35 @@
 
 const arithmetic = require('../lib/arithmetic.js');
 
+describe('Arithmetic', () => {
+    
+    describe('testing .add function', () => {
+
+        it('returns sum of two numbers ', () => {
+            let input = arithmetic.add(1,2);
+            let expectedOutput = 3;
+            expect(input).toEqual(expectedOutput);
+        })
+
+        it('returns null if passed not-a-number', () => {
+            let input = arithmetic.add('1',2);
+            let expectedOutput = null;
+            expect(input).toEqual(expectedOutput);
+        })
+
+    })
+
+    describe('testing .subtract function', () => {
+
+        it('returns difference of two numbers', () => {
+            let input = arithmetic.subtract(3,2);
+            let expectedOutput = 1;
+            expect(input).toEqual(expectedOutput);
+        })
+        it('returns null if passed not-a-number', () => {
+            let input = arithmetic.subtract('3',2);
+            let expectedOutput = null;
+            expect(input).toEqual(expectedOutput);
+        })
+    })
+})
